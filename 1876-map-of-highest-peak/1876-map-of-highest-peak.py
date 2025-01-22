d3307e591a591462fcc04c55b6b888e5018522e7
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-01-22 20:08:04 +0300

    Time: 1748 ms (22.12%), Space: 221.8 MB (22.59%) - LeetHub

---
+++ 1876-map-of-highest-peak/1876-map-of-highest-peak.py
@@ -0,0 +1,31 @@
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        q=deque()
+        visit=set()
+        res=[[-1]*len(isWater[0]) for _ in range(len(isWater))]
+        
+        for r in range(len(isWater)):
+            for c in range(len(isWater[0])):
+                if isWater[r][c]:
+                    res[r][c]=0
+                    q.append((r , c))
+                    visit.add((r , c))
+        #neighbor
+        while q:
+            r,c=q.popleft()
+            h=res[r][c]
+            ng=[[r+1 , c] , [r , c+1] , [r-1 , c] , [r , c-1]]
+            for nr , nc in ng:
+                if (nr<0 or nc<0 or nc>=len(isWater[0] ) or nr>=len(isWater)) or (nr , nc) in visit:
+                    continue
+
+                q.append((nr , nc))
+                visit.add((nr , nc))
+                res[nr][nc]=h+1
+        return res
+        print(q)
+
+              
+
+        
+
