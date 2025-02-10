3a959a0e97151b72c9f07560353f04f250c88bae
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-10 21:11:01 +0300

    Time: 764 ms (20.25%), Space: 23.6 MB (99.86%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,32 +1,33 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        def merge_sort(nums):
-            if  len(nums) > 1:
-                mid = len(nums) // 2
-                left_array = nums[:len(nums)//2]
-                right_array = nums[len(nums)//2:]
-                merge_sort(left_array)
-                merge_sort(right_array)
-                i = j = k = 0
-                while i < len(left_array) and j < len(right_array):
-                    if left_array[i] <= right_array[j]:
-                        nums[k] = left_array[i]
-                        i += 1
-                    else:
-                        nums[k] = right_array[j]
-                        j += 1
-                    k += 1
+        result=[]
+        def mergesort(nums , result):
+            
+            if len(nums)>1:
+                mid=len(nums)//2
+                lft=nums[:mid]
+                rght=nums[mid:]
+                mergesort(lft , result)
+                mergesort(rght , result)
+                i=j=k=0
+                while i<len(lft) and j<len(rght):
+                    if lft[i]<=rght[j]:
+                        nums[k]=lft[i]
+                        i+=1
 
-                while i < len(left_array):
-                    nums[k] = left_array[i]
-                    i += 1
-                    k += 1
-
-                while j < len(right_array):
-                    nums[k] = right_array[j]
-                    j += 1
-                    k += 1
-                
+                    elif lft[i]>=rght[j]:
+                        nums[k]=rght[j]
+                        j+=1
+                    k+=1
+                while i<len(lft):
+                    nums[k]=lft[i]
+                    i+=1
+                    k+=1
+                while j<len(rght):
+                    nums[k]=rght[j]
+                    j+=1
+                    k+=1
             return nums
-
-        return merge_sort(nums)
+        return mergesort(nums , result)
+                    
+                
