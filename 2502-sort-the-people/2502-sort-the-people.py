7b42e209b0b4e85a4eb02128bb2eae20d1e999eb
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-10 10:43:07 +0300

    Time: 223 ms (10.63%), Space: 18 MB (82.01%) - LeetHub

--- 2502-sort-the-people/2502-sort-the-people.py
+++ 2502-sort-the-people/2502-sort-the-people.py
@@ -1,12 +1,19 @@
 class Solution:
     def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
-        hsh={}
-        for i  , value in enumerate(heights):
-            hsh[value]=names[i]
-        print(hsh)
-        hsh=sorted(hsh.items() , reverse=True)
-        print(hsh)
-        resu=[]
-        for i , key in hsh:
-            resu.append(key)
-        return resu
+        # for i in range(len(heights)):
+        #     swp=False
+        #     for j in range(len(heights)-i-1):
+        #         if heights[j]>heights[j+1]:
+        #             heights[j] , heights[j+1]=heights[j+1] , heights[j]
+        #             swp=True
+        #         if not swp:
+        #             break
+        # print(heights)
+        for i in range(len(heights)):
+            mini=i
+            for j in range(i+1 , len(heights)):
+                if heights[j]<heights[mini]:
+                    mini=j
+            heights[i] , heights[mini]=heights[mini] , heights[i]
+            names[i] , names[mini]=names[mini] , names[i]
+        return names[::-1]
