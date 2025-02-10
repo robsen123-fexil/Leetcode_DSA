c219ee5a05a7fcf6ca006a1c67bf1a729018a7cd
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-10 20:59:45 +0300

    Time: 1154 ms (5.02%), Space: 23.9 MB (95.25%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.py
+++ 0948-sort-an-array/0948-sort-an-array.py
@@ -1,13 +1,32 @@
 class Solution:
     def sortArray(self, nums: List[int]) -> List[int]:
-        maximum = max(nums)
-        minimum = min(nums)
-        count = [0] * (maximum - minimum + 1)  
-        for num in nums:
-            count[num - minimum] += 1
-        target = 0
-        for index, value in enumerate(count):
-            for i in range(value):
-                nums[target] = index + minimum
-                target += 1
-        return nums
+        def merge_sort(nums):
+            if  len(nums) > 1:
+                mid = len(nums) // 2
+                left_array = nums[:len(nums)//2]
+                right_array = nums[len(nums)//2:]
+                merge_sort(left_array)
+                merge_sort(right_array)
+                i = j = k = 0
+                while i < len(left_array) and j < len(right_array):
+                    if left_array[i] <= right_array[j]:
+                        nums[k] = left_array[i]
+                        i += 1
+                    else:
+                        nums[k] = right_array[j]
+                        j += 1
+                    k += 1
+
+                while i < len(left_array):
+                    nums[k] = left_array[i]
+                    i += 1
+                    k += 1
+
+                while j < len(right_array):
+                    nums[k] = right_array[j]
+                    j += 1
+                    k += 1
+                
+            return nums
+
+        return merge_sort(nums)
