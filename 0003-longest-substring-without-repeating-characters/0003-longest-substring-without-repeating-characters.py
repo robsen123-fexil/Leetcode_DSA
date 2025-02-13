9a74f6ab1c0fb7f9a117dad20d3d073be6d6a81e
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-13 10:40:41 +0300

    Time: 47 ms (15.35%), Space: 17.8 MB (50.74%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,12 +3,14 @@ class Solution:
         cnt=Counter()
         maxima=float('-inf')
         l=0
+        if not s:
+            return 0
         for i in range(len(s)):
             cnt[s[i]]+=1
-            while cnt[s[i]]>1:
+            while cnt[s[i]]>=2:
                 cnt[s[l]]-=1
                 if cnt[s[l]]==0:
-                    cnt.pop(s[l])
+                    del cnt[s[l]]
                 l+=1
-            maxima=max(maxima , i-l+1)
-        return maxima if maxima!=float('-inf') else 0
+            maxima=max(maxima , len(cnt))
+        return maxima
