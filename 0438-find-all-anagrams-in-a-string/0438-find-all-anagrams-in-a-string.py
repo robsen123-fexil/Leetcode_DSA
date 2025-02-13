279caeccff8691f3d0f4d54aa0387e8fbca4fd72
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-13 12:01:44 +0300

    Time: 229 ms (27.45%), Space: 18.8 MB (5.6%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.py
@@ -4,14 +4,16 @@ class Solution:
         l=0
         frst=s[:len(p)]
 
-        if sorted(frst)==sorted(p):
+        cnt1=Counter(p)
+        cnt2=Counter(frst)
+        if cnt1==cnt2:
             result.append(0)
-        frst=list(frst)
         for i in range(len(p) , len(s)):
-            frst.append(s[i])
-            frst.remove(s[l])
-            
-            if sorted(''.join(frst))==sorted(p):
+            cnt2[s[l]]-=1
+            if cnt2[s[i]]==0:
+                del cnt2[s[i]]
+            cnt2[s[i]]+=1
+            if cnt2==cnt1:
                 result.append(l+1)
             l+=1
         return result
