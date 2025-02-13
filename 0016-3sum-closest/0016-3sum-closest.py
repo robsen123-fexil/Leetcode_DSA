ee61dda06001316d38250f898d7e6101568dc650
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-13 18:45:58 +0300

    Time: 545 ms (7.99%), Space: 23.5 MB (9.35%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.py
+++ 0016-3sum-closest/0016-3sum-closest.py
@@ -1,19 +1,38 @@
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        minima=float('inf')
         nums.sort()
-        for i in range(len(nums)-2):
+        hsh=defaultdict(list)
+        for i in range(len(nums)):
             l=i+1
             r=len(nums)-1
             while l<r:
-                res=nums[i]+nums[l]+nums[r]
-                if abs(target-res) < abs(target-minima):
-                    minima=res
-                if res>target:
-                    r-=1
-                elif res<target:
+                sums=nums[i]+nums[l]+nums[r]
+                clo=abs(sums-target)
+                hsh[clo].append(sums)
+                if sums<target:
                     l+=1
+                elif sums>target:
+                    r-=1
                 else:
-                    return res
-        return minima if minima!=float('inf') else 0
+                    return sums
+        mini=min(hsh.keys())
+        return max(hsh[mini])
+
+
+        # minima=float('inf')
+        # nums.sort()
+        # for i in range(len(nums)-2):
+        #     l=i+1
+        #     r=len(nums)-1
+        #     while l<r:
+        #         res=nums[i]+nums[l]+nums[r]
+        #         if abs(target-res) < abs(target-minima):
+        #             minima=res
+        #         if res>target:
+        #             r-=1
+        #         elif res<target:
+        #             l+=1
+        #         else:
+        #             return res
+        # return minima if minima!=float('inf') else 0
 
