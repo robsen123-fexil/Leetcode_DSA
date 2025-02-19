ed7b54f0b6628114f62a4c4ec3a97130f95cfa57
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-02-19 16:36:16 +0300

    Time: 32 ms (70.33%), Space: 31.7 MB (39.09%) - LeetHub

--- 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
+++ 1477-product-of-the-last-k-numbers/1477-product-of-the-last-k-numbers.py
@@ -2,14 +2,21 @@ class ProductOfNumbers:
 
     def __init__(self):
         self.lists=[]
+        self.ans = [1]
         
 
     def add(self, num: int) -> None:
         self.lists.append(num)
+        if num == 0:
+            self.ans = [1]
+        else:
+            self.ans.append(self.ans[-1] * num)
         
  
     def getProduct(self, k: int) -> int:
-        return math.prod(self.lists[-k:])
+        dv=(k+1)
+        return self.ans[-1]//self.ans[-dv] if k<(len(self.ans)) else 0 
+            
         
 
 
