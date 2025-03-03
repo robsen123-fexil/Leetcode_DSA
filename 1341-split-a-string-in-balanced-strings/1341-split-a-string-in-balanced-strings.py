8e589c64d444513600081818e3f5fc8a6ac09502
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-03-03 20:40:48 +0300

    Time: 0 ms (100%), Space: 17.6 MB (80.72%) - LeetHub

--- 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
+++ 1341-split-a-string-in-balanced-strings/1341-split-a-string-in-balanced-strings.py
@@ -1,11 +1,11 @@
 class Solution:
     def balancedStringSplit(self, s: str) -> int:
-        l=r=count=0
+        sums=count=0
         for i in s:
             if i=='R':
-                r+=1
-            elif i=='L':
-                l+=1
-            if r==l:
+                sums+=(-1)
+            else:
+                sums+=(1)
+            if sums==0:
                 count+=1
         return count
