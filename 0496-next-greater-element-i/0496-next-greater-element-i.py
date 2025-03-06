02bed606717ccfcd3174f09a0133ba66d2f66e1d
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-03-06 18:06:35 +0300

    Time: 0 ms (100%), Space: 18 MB (65.36%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -2,17 +2,16 @@ class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         hsh={}
         stack=[]
-        frsts={}
+        frsts=defaultdict(int)
         for i , val in enumerate(nums2):
             while stack and stack[-1]<val:
                 b=stack.pop()
                 frsts[b]=val
             stack.append(val)
         ans=[]
-        for i in nums2:
-            if i not in frsts:
-                frsts[i]=-1
         for i in nums1:
-            ans.append(frsts[i])
+            if not frsts[i]:
+                ans.append(-1)
+            else:
+                ans.append(frsts[i])
         return ans
-
