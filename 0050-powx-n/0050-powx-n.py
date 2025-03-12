336a65357a9241cb42c30737989cf69f93ca074e
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-03-12 10:58:28 +0300

    Time: 1 ms (12.6%), Space: 17.8 MB (50.35%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,3 +1,14 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        return x**n
+        def solver(x , n):
+            if n==0:
+                return 1
+            frsts=solver(x , n//2)
+            if n%2==0:
+                return frsts*frsts
+            else:
+                return x*frsts*frsts
+        ans=solver(x , abs(n))
+        if n<0:
+            return 1/ans
+        return ans
