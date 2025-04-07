d263c28f56d8e39cafbe57cbd0324b1e1b3c3807
Robsen Teshome <111446547+robsen123-fexil@users.noreply.github.com>
2025-04-07 18:24:17 +0300

    Time: 19 ms (97.47%), Space: 18.1 MB (83.99%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.py
+++ 0463-island-perimeter/0463-island-perimeter.py
@@ -1,19 +1,12 @@
 class Solution:
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        rows, cols = len(grid), len(grid[0])
-        perimeter = 0
-
-        for r in range(rows):
-            for c in range(cols):
-                if   grid[r][c] == 1:
-                    perimeter += 4
-
-                    # Check above
-                    if r > 0 and grid[r-1][c] == 1:
-                        perimeter -= 2  # one shared edge (up and down)
-                    
-                    # Check left
-                    if c > 0 and grid[r][c-1] == 1:
-                        perimeter -= 2  # one shared edge (left and right)
-
-        return perimeter
+        ans=0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j]==1:
+                    ans+=4
+                    if i>0 and grid[i-1][j]==1:
+                        ans-=2
+                    if j>0 and grid[i][j-1]==1:
+                        ans-=2
+        return ans
